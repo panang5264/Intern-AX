c3b5dacf02a071b6a6a2cf38e9dd9c877343dab8
HeHHeyboi <thanathatwave@gmail.com>
2026-05-08 17:41:43 +0700

    Start Refactor Tower
    refact: detection_area to apply on Tower & Enemy
    new: Physic Grop Tower, EnemyDetect, TowerDetect
    new: Tower Prefab
    - SplashTower
    - SupportTower
    - NormalTower

--- assets/Test/tower.ts
+++ assets/Test/tower.ts
@@ -1,4 +1,4 @@
-import { _decorator, assert, Component, Node, Vec3, math, EventTarget, Prefab, Quat, instantiate, CCFloat, director } from 'cc';
+import { _decorator, assert, Component, Node, Vec3, math, EventTarget, Prefab, Quat, instantiate, CCFloat, director, CCBoolean } from 'cc';
 import { DetectionArea, DetectionType } from './detection_area';
 import { Bullet } from './bullet'
 const { ccclass, property } = _decorator;
@@ -17,28 +17,36 @@ export class Tower extends Component {
     public cooldown = 0.5;
     private cur_cooldown = 0;
 
-    public onDetectEnemyBound: (node: Node) => void;
-    public onEnemyLeaveBound: (node: Node) => void;
+    @property({
+        type: CCBoolean,
+        displayName: "Support"
+    })
+    public is_support = false;
+
+    public onInTowerRange: (node: Node) => void;
+    public onOutTowerRange: (node: Node) => void;
 
     protected start(): void {
         if (this.detection_area == null) {
             this.detection_area = this.node.getComponentInChildren(DetectionArea)
         }
         assert(this.enemy_list != null, "enemy_list is null or undefine")
-        assert(this.bullet != null, "insert bullet prefab")
+        if (!this.is_support) {
+            assert(this.bullet != null, "insert bullet prefab")
+        }
 
-        this.onDetectEnemyBound = this.onDetectEnemy.bind(this);
-        this.onEnemyLeaveBound = this.onEnemyLeave.bind(this)
-        this.detection_area.addListener(DetectionType.Enter, this.onDetectEnemyBound);
-        this.detection_area.addListener(DetectionType.Leave, this.onEnemyLeaveBound);
+        this.onInTowerRange = this.onInRange.bind(this);
+        this.onOutTowerRange = this.onOutOfRange.bind(this)
+        this.detection_area.addListener(DetectionType.Enter, this.onInTowerRange);
+        this.detection_area.addListener(DetectionType.Leave, this.onOutTowerRange);
     }
 
-    public onDetectEnemy(enemy: Node) {
+    public onInRange(enemy: Node) {
         // this.node.lookAt(enemy.worldPosition, Vec3.UP)
         this.enemy_list.push(enemy)
     }
 
-    public onEnemyLeave(enemy: Node) {
+    public onOutOfRange(enemy: Node) {
         const i = this.enemy_list.indexOf(enemy)
         if (i !== -1) {
             this.enemy_list.splice(i, 1)
@@ -46,8 +54,8 @@ export class Tower extends Component {
     }
 
     protected onDisable(): void {
-        this.detection_area.removeListener(DetectionType.Enter, this.onDetectEnemyBound)
-        this.detection_area.removeListener(DetectionType.Leave, this.onEnemyLeaveBound)
+        this.detection_area.removeListener(DetectionType.Enter, this.onInTowerRange)
+        this.detection_area.removeListener(DetectionType.Leave, this.onOutTowerRange)
     }
 
     protected update(dt: number): void {
