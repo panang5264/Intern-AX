982b0932c2b2964ed57e56ac461d1ec34d6afd1f
HeHHeyboi <thanathatwave@gmail.com>
2026-05-08 12:01:55 +0700

    Implement WaveManager to manage wave
    new: WaveData hold enemy amt., spawn freq., enemyPrefab
    new: Wave hold current wave time, data index

--- assets/GameCode/Monsters/EnemyManager.ts
+++ assets/GameCode/Monsters/EnemyManager.ts
@@ -3,41 +3,62 @@ import { PathManager } from '../Stages/PathManager';
 import { EnemyMovement } from './EnemyMovement';
 const { ccclass, property } = _decorator;
 
-enum EnemyStage {
+enum WaveStatus {
     Spawn,
     Wait,
 }
 
 @ccclass('WaveData')
 export class WaveData {
-
-    @property
+    @property(
+        {
+            displayName: "Enemy Amt.",
+            range: [1, 99, 1],
+        }
+    )
     enemy_num: number = 0;
 
-    @property
-    spawn_freq: number = 0;
-
-    cur_spawn_time: number = 0;
+    @property(
+        {
+            displayName: "Spawn Freq.",
+            tooltip: "Spawn Enemy in sec.",
+            range: [0.1, 10, 0.1],
+            slide: true,
+        }
+    )
+    spawn_freq: number = 0.5;
+    @property(Prefab) enemyPrefab: Prefab = null;
 }
 
-@ccclass('EnemyManager')
-export class EnemyManager extends Component {
-    @property(Prefab) enemyPrefab: Prefab = null
+@ccclass('Wave')
+export class Wave {
+    @property([WaveData]) wave_data: WaveData[] = []
+    cur_time: number = 0;
+    idx: number = 0;
+    get_data(): WaveData {
+        return this.wave_data[this.idx]
+    }
+    is_done(): boolean {
+        return this.idx >= this.wave_data.length
+    }
+}
+
+
+@ccclass('WaveManager')
+export class WaveManager extends Component {
     @property(PathManager) path_manager: PathManager = null
-    @property([WaveData])
-    waves: WaveData[] = [];
+    @property([Wave]) waves: Wave[] = [];
     wave_idx = 0;
 
     @property(Node) spawnPoint: Node = null;
     @property(CCFloat) duration: number = 5.0;
     cur_duration: number = 0.0;
-    cur_stage: EnemyStage = EnemyStage.Wait;
+    cur_stage: WaveStatus = WaveStatus.Wait;
     cur_spawn_time: number = 0.0;
 
     protected start(): void {
-        assert(this.enemyPrefab !== null, "enemy prefab is null")
-        assert(this.spawnPoint !== null, "didn't set enemy spawn Point")
-        assert(this.path_manager !== null, "didn't set PathManager for EnemyManager")
+        assert(this.spawnPoint !== null, "didn't set enemy spawn Point for WaveManager")
+        assert(this.path_manager !== null, "didn't set PathManager for WaveManager")
     }
 
     protected update(dt: number): void {
@@ -46,37 +67,40 @@ export class EnemyManager extends Component {
         }
 
         switch (this.cur_stage) {
-            case EnemyStage.Spawn:
-                console.log("spawn")
-                let wave = this.waves[this.wave_idx]
-                wave.cur_spawn_time += dt;
-                if (wave.cur_spawn_time >= wave.spawn_freq) {
-                    wave.enemy_num -= 1;
-                    wave.cur_spawn_time = 0;
-                    let node = instantiate(this.enemyPrefab)
-                    let enemyMovement = node.getComponent(EnemyMovement)
-                    enemyMovement.set_path_manager(this.path_manager)
-                    node.setWorldPosition(this.spawnPoint.worldPosition)
-                    node.setParent(this.node, true)
+            case WaveStatus.Spawn:
+                const wave = this.waves[this.wave_idx];
+                if (wave.is_done()) {
+                    this.cur_stage = WaveStatus.Wait;
+                    this.wave_idx += 1;
+                    break;
                 }
 
-                if (wave.enemy_num <= 0) {
-                    this.cur_stage = EnemyStage.Wait
-                    this.wave_idx += 1;
+                const data = wave.get_data()
+                assert(data != null, "Data is null")
+                wave.cur_time += dt;
+                if (wave.cur_time >= data.spawn_freq) {
+                    wave.cur_time = 0;
+                    data.enemy_num -= 1;
+                    let node = instantiate(data.enemyPrefab)
+                    node.setParent(this.node, true)
+                    node.setWorldPosition(this.spawnPoint.worldPosition)
+                    let enemy = node.getComponent(EnemyMovement)
+                    enemy.pathManager = this.path_manager
+
+                }
+
+                if (data.enemy_num <= 0) {
+                    wave.idx += 1;
                 }
                 break;
-            case EnemyStage.Wait:
+            case WaveStatus.Wait:
                 console.log("wait")
                 this.cur_duration += dt;
                 if (this.cur_duration >= this.duration) {
-                    this.cur_stage = EnemyStage.Spawn
+                    this.cur_stage = WaveStatus.Spawn
                     this.cur_duration = 0;
                 }
                 break;
         }
-
-
     }
 }
-
-
